4f176ed0810bbea38ebbad244d3dfc815d1fc7f3
muzziewuzzie <musavvirmuzakkir@gmail.com>
2022-04-19 21:38:21 -0700

    Created NavBar component

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <ul className="nav_links">
+        <li>Home</li>
+        <li>Topics</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;

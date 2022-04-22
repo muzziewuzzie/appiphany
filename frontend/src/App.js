7534fcd54283ff49bf58f1a715589e82907d5057
muzziewuzzie <musavvirmuzakkir@gmail.com>
2022-04-22 10:50:26 -0700

    Created home page (1st iteration)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 // import "./App.css";
 import "./styles.css";
 import Header from "./components/Header";
+import HomepageContent from "./components/HomepageContent";
 
 const App = () => {
   return (
-    <>
+    <div className="app">
       <Header />
-    </>
+      <HomepageContent />
+    </div>
   );
 };
 
---
+++ frontend/src/components/HomepageContent.js
@@ -0,0 +1,17 @@
+import Button from "./Button";
+
+const HomepageContent = () => {
+  return (
+    <div className="homepage_content">
+      <div className="content">
+        <h1 className="content_title">RACISM.</h1>
+        <p className="content_details">
+          Let's not remain ignorant any longer. Break the stigma now.
+        </p>
+        <Button name="Learn More" />
+      </div>
+    </div>
+  );
+};
+
+export default HomepageContent;

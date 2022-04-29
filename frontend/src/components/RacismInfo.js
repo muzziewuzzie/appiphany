aa2c59ab81b88b1613a3463f1351fbaae2fac942
muzziewuzzie <musavvirmuzakkir@gmail.com>
2022-04-29 11:22:22 -0700

    Seperated info to another file

---
+++ frontend/src/components/RacismInfo.js
@@ -0,0 +1,84 @@
+const information = [
+  <div className="racism_information">
+    <h2>Brief History</h2>
+    <p className="information">
+      When discussing slavery, Greek philosopher, Aristotle, stated that Greeks
+      are free by nature, and non-Greeks are slaves by nature. Non-Greeks were
+      also referred to as "barbarians" who have strong bodies but are not
+      intelligent, thus making them unfit for leadership positions.
+    </p>
+  </div>,
+  <div className="racism_information">
+    <h2>Brief History</h2>
+    <p className="information">
+      "Proto-racism" is defined as actions that display prejudice and
+      stereotypes to a certain group at a time which predates the modern
+      biologically-based concept of race. Aristotle's discussion of slavery is
+      an example of proto-racism, and according to Benjamin Issac, professor of
+      Ancient History Emeritus at Tel Aviv University, it is an important
+      precursor to mordern racism.
+    </p>
+  </div>,
+  <div className="racism_information">
+    <h2>Brief History</h2>
+    <p className="information">
+      Such proto-racism and ethnicity-based prejudice that is based on
+      Aristotle's identification can be seen in multiple historical events. Such
+      historical events include Anglo Saxons believing their pursuit of liberty
+      is incomparable to other nations, making them superior to other races, as
+      well as the holocaust where the idea of a "pure" race should survive.
+      While the world has made strides in eliminating such ideologies, racism
+      continue to exist in different ways currently.
+    </p>
+  </div>,
+  <div className="racism_information">
+    <h2>Presence in Today's Context</h2>
+    <p className="information">
+      Racism is not as overt as it used to be, but it exists in some form, which
+      is usually along the lines of "prejudice" - a preconceived opinion that is
+      not based on reason. In the U.S., Megan Francis shared about the time she
+      almost missed her flight when security guards accused her of bringing a
+      weapon into the airport. The "weapon" was merely a plastic ring, to which
+      the security guards insisted that she was lying and that "people of her
+      kind are dangerous". In the U.K., Chuks Nwaobasi shared about incidents
+      when he used to be on the receiving end of racist behaviour, like name
+      calling. When he tried bringing attention to this matter, he received
+      comments such as "it does not happen that often", or "you\'re being too
+      sensitive". Such prejudicial behaviour can also been in Singapore, where a
+      30-year-old Chinese man physical assaulted and made an offensive racial
+      slur to an Indian woman.',
+    </p>
+  </div>,
+  <div className="racism_information">
+    <h2>Presence in Today's Context</h2>
+    <p className="information">
+      There are a few theories as to why racism exist in today's context. One
+      such theory is scapegoat theory, where prejudice is used as a defense
+      mechanism by frustrated people who blame a more disadvantaged group for
+      the troubles they face, even when such troubles could stem from structural
+      changes. For example, blaming immigrants for losing jobs, rather than
+      looking at how globalisation/automation could have changed the economy.
+      While such prejudicial thoughts are less than desirable, a respectful
+      conversation between two people would make all the difference in setting
+      these thoughts right. However, there are also difficulties in how
+      different races groups interact.
+    </p>
+  </div>,
+  <div className="racism_information">
+    <h2>Difficulties in Interacting</h2>
+    <p className="information">
+      Among some of the categories sociologists have termed how different racial
+      groups interact, assimilation and segregation is two of them. Assimilation
+      is when minorities gradually adopt patterns of a dominant culture, like
+      fashion or language spoken. This way minorities are sometimes able to
+      avoid prejudice/discrimination, but this is sometimes difficult for some.
+      It is only easier when the minority group looks or sounds similar to the
+      majority group. When it is difficult, interacting becomes a chore, and
+      this is where segregation comes in. Segregation is the phsyical and social
+      seperation of categories of people, where people work and interact with
+      others who are of the same race.",
+    </p>
+  </div>,
+];
+
+export default information;
